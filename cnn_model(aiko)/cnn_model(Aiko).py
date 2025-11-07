c109bada0291f8dbe9e5e462f25471550704d745
rishi-naath <rishinaath33@gmail.com>
2025-11-07 18:11:38 +0530

    the brain

--- cnn_model(aiko)/cnn_model(Aiko).py
+++ cnn_model(aiko)/cnn_model(Aiko).py
@@ -26,11 +26,11 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy']
 # Data augmentation
 datagen = ImageDataGenerator(
     rescale=1./255,
-    validation_split=0.2,
-    rotation_range=40,
+    validation_split=0.2, #eg: 100 photos ==> learns 80, tested by 20.
+    rotation_range=40, # + or - 40 degrees.
     width_shift_range=0.4,
     height_shift_range=0.4,
-    shear_range=0.4,
+    shear_range=0.4, #injects blur for smooth detection.
     zoom_range=0.4,
     horizontal_flip=True,
     fill_mode='nearest'
@@ -52,13 +52,13 @@ lr_scheduler = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=3)
 # Use 8-10 epochs if your system can't handle more runs in a single run. 
 history = model.fit(train, validation_data=val, epochs=25, callbacks=[early_stopping, lr_scheduler])
 
-# Save the model in HDF5 format (whyyy literally whyyy i don't know, but let it be here)
+# Save the model in HDF5 format (whyyy literally whyyy i don't know, i don't use it, but let it be here)
 model.save('cnn_model.h5')
 
 # Save the model in SavedModel format
-model.save(r'path\to\save\your\model')  # Save the model in a directory and name it as 'model_name.keras'
+model.save(r'path\to\save\your\model_name.keras')  # Save the model in a directory and name it as 'model_name.keras'
 
-# Visualize the CNN architecture (the flowchart that i can't seem to understand,sed)
+# Visualize the CNN architecture (the flowchart that I can't seem to understand, sed)
 plot_model(model, to_file='cnn_model_graph.png', show_shapes=True, show_layer_names=True)
 
 # Plot training and validation loss (bring a bunch of friends and gamble on this)
@@ -70,3 +70,4 @@ plt.legend()
 plt.title('Training and Validation Loss')
 
 plt.show()
+
